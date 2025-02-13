bb06a834bca7bf1a4effe5127cdc8617410092bb
Kaio Matos <kaio_matos22@hotmail.com>
2025-02-12 21:57:50 -0300

    add a pseudorandom number generator

--- basis/src/lib.rs
+++ basis/src/lib.rs
@@ -5,6 +5,15 @@ pub fn add(left: u64, right: u64) -> u64 {
     left + right
 }
 
+// Pseudorandom number generator from the "Xorshift RNGs" paper by George Marsaglia.
+pub fn gen_u32(num: u32) -> u32 {
+    let mut random = num;
+    random ^= random << 13;
+    random ^= random >> 17;
+    random ^= random << 5;
+    random
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
