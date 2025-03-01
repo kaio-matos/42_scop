dfc6a925b239f38e63e49f04e307dd58f3c4d2d2
Kaio Matos <kaio_matos22@hotmail.com>
2025-02-28 21:02:44 -0300

    refactor how events will be handled

--- basis/src/graphics/window.rs
+++ basis/src/graphics/window.rs
@@ -1,9 +1,23 @@
-use glfw::{Action, Context, GlfwReceiver, Key, WindowEvent};
+use std::collections::HashMap;
 
+use glfw::{Action, Context, GlfwReceiver, Key, Modifiers, WindowEvent};
+
+#[derive(Debug, Hash, Eq, PartialEq)]
+struct KeyEvent {
+    pub key: Key,
+    pub modifiers: Modifiers,
+}
+
+#[derive(Debug)]
 pub struct Window {
     pub glfw: glfw::Glfw,
+    pub deltatime: f32,
+    pub events: Vec<WindowEvent>,
+
+    hold_keys: HashMap<KeyEvent, bool>,
     window_handle: glfw::PWindow,
-    events: GlfwReceiver<(f64, WindowEvent)>,
+    raw_events: GlfwReceiver<(f64, WindowEvent)>,
+    last_frame: f32,
 }
 
 impl Window {
@@ -19,9 +33,20 @@ impl Window {
 
         Window {
             glfw,
+            deltatime: 0.0,
+            events: Vec::default(),
+
+            hold_keys: HashMap::default(),
             window_handle: window,
-            events,
+            raw_events: events,
+            last_frame: 0.0,
         }
+    }
+
+    pub fn compute_deltatime(&mut self) {
+        let current_frame = self.glfw.get_time() as f32;
+        self.deltatime = current_frame - self.last_frame;
+        self.last_frame = current_frame;
     }
 
     pub fn init_gl(&mut self) {
@@ -42,17 +67,73 @@ impl Window {
         self.window_handle.swap_buffers();
     }
 
+    pub fn on_key_release(&mut self, key: glfw::Key, modifiers: glfw::Modifiers) -> bool {
+        let found = self.events.iter().find(|event| {
+            match event {
+                WindowEvent::Key(k, _, action, m) => {
+                    if k == &key && m == &modifiers && action == &Action::Release {
+                        return true;
+                    }
+                }
+                _ => {}
+            }
+            false
+        });
+        found.is_some()
+    }
+
+    pub fn on_key_press(&mut self, key: glfw::Key, modifiers: glfw::Modifiers) -> bool {
+        let found = self.events.iter().find(|event| {
+            match event {
+                WindowEvent::Key(k, _, action, m) => {
+                    if k == &key && m == &modifiers && action == &Action::Press {
+                        return true;
+                    }
+                }
+                _ => {}
+            }
+            false
+        });
+        found.is_some()
+    }
+
+    pub fn on_key_hold(&mut self, key: glfw::Key, modifiers: glfw::Modifiers) -> bool {
+        let is_pressing = self.hold_keys.get(&KeyEvent { key, modifiers });
+
+        match is_pressing {
+            Some(value) => *value,
+            None => false,
+        }
+    }
+
     fn process_events<F>(&mut self, on_event: &mut F)
     where
         F: FnMut(&WindowEvent),
     {
-        for (_, event) in glfw::flush_messages(&self.events) {
+        self.events.clear();
+        for (_, event) in glfw::flush_messages(&self.raw_events) {
+            self.events.push(event.clone());
+            match event {
+                glfw::WindowEvent::Key(k, _, action, m) => {
+                    let key = KeyEvent {
+                        key: k,
+                        modifiers: m,
+                    };
+
+                    if action == Action::Press {
+                        self.hold_keys.insert(key, true);
+                    } else if action == Action::Release {
+                        self.hold_keys.insert(key, false);
+                    }
+                }
+                _ => {}
+            }
             on_event(&event);
             match event {
                 glfw::WindowEvent::FramebufferSize(width, height) => unsafe {
                     gl::Viewport(0, 0, width, height)
                 },
-                glfw::WindowEvent::Key(Key::Escape, _, Action::Press, _) => {
+                glfw::WindowEvent::Key(Key::Escape, _, glfw::Action::Press, _) => {
                     self.window_handle.set_should_close(true)
                 }
                 _ => {}
