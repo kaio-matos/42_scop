81eb95d0363ece0849f12db418ce7614a6503f42
Kaio Matos <kaio_matos22@hotmail.com>
2025-04-09 22:18:51 -0300

    enable cube structs to move

--- scop/src/structs/cube.rs
+++ scop/src/structs/cube.rs
@@ -12,20 +12,32 @@ pub struct Cube {
 
 impl Controllable for Cube {
     fn get_speed(&self, deltatime: f32) -> f32 {
-        100. * deltatime
+        30. * deltatime
     }
 
-    fn move_forward(&mut self, _deltatime: f32) {}
+    fn move_forward(&mut self, deltatime: f32) {
+        self.object.position.z -= self.get_speed(deltatime);
+    }
 
-    fn move_backward(&mut self, _deltatime: f32) {}
+    fn move_backward(&mut self, deltatime: f32) {
+        self.object.position.z += self.get_speed(deltatime);
+    }
 
-    fn move_left(&mut self, _deltatime: f32) {}
+    fn move_left(&mut self, deltatime: f32) {
+        self.object.position.x -= self.get_speed(deltatime);
+    }
 
-    fn move_right(&mut self, _deltatime: f32) {}
+    fn move_right(&mut self, deltatime: f32) {
+        self.object.position.x += self.get_speed(deltatime);
+    }
 
-    fn move_up(&mut self, _deltatime: f32) {}
+    fn move_up(&mut self, deltatime: f32) {
+        self.object.position.y += self.get_speed(deltatime);
+    }
 
-    fn move_down(&mut self, _deltatime: f32) {}
+    fn move_down(&mut self, deltatime: f32) {
+        self.object.position.y -= self.get_speed(deltatime);
+    }
 
     fn rotate(&mut self, _deltatime: f32, _yaw: f32, _pitch: f32) {}
 
@@ -43,6 +55,40 @@ impl EntityLifetime for Cube {
 
     fn update(&mut self, window: &mut basis::graphics::window::Window) {
         if window.on_key_hold(graphics::glfw::Key::Up, graphics::glfw::Modifiers::empty()) {
+            self.move_up(window.deltatime)
+        }
+        if window.on_key_hold(
+            graphics::glfw::Key::Down,
+            graphics::glfw::Modifiers::empty(),
+        ) {
+            self.move_down(window.deltatime)
+        }
+        if window.on_key_hold(
+            graphics::glfw::Key::Left,
+            graphics::glfw::Modifiers::empty(),
+        ) {
+            self.move_left(window.deltatime)
+        }
+        if window.on_key_hold(
+            graphics::glfw::Key::Right,
+            graphics::glfw::Modifiers::empty(),
+        ) {
+            self.move_right(window.deltatime)
+        }
+        if window.on_key_hold(
+            graphics::glfw::Key::PageUp,
+            graphics::glfw::Modifiers::empty(),
+        ) {
+            self.move_forward(window.deltatime)
+        }
+        if window.on_key_hold(
+            graphics::glfw::Key::PageDown,
+            graphics::glfw::Modifiers::empty(),
+        ) {
+            self.move_backward(window.deltatime)
+        }
+
+        if window.on_key_hold(graphics::glfw::Key::Up, graphics::glfw::Modifiers::Control) {
             self.rotateq(
                 window.deltatime,
                 Quaternion::from_euler_angles(math::Vec3::new(0.1, 0.0, 0.0), 5_f32.to_radians()),
@@ -51,7 +97,7 @@ impl EntityLifetime for Cube {
 
         if window.on_key_hold(
             graphics::glfw::Key::Down,
-            graphics::glfw::Modifiers::empty(),
+            graphics::glfw::Modifiers::Control,
         ) {
             self.rotateq(
                 window.deltatime,
@@ -60,7 +106,7 @@ impl EntityLifetime for Cube {
         }
         if window.on_key_hold(
             graphics::glfw::Key::Left,
-            graphics::glfw::Modifiers::empty(),
+            graphics::glfw::Modifiers::Control,
         ) {
             self.rotateq(
                 window.deltatime,
@@ -69,7 +115,7 @@ impl EntityLifetime for Cube {
         }
         if window.on_key_hold(
             graphics::glfw::Key::Right,
-            graphics::glfw::Modifiers::empty(),
+            graphics::glfw::Modifiers::Control,
         ) {
             self.rotateq(
                 window.deltatime,
